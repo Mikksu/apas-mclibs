5cb9f0cea37f237648f9833b8ccc9ec3f4f8fb63
SL <123@123.com>
2024-10-20 22:43:22 +0800

    refactor: 优化ACS单元测试代码。
    增加目标控制器PortName定义，支持连接到模拟器进行单元测试。

--- APAS.MotionLibZMC/APAS.MotionLib.ZMCTests1/Zmc4SeriesTests.cs
+++ APAS.MotionLibZMC/APAS.MotionLib.ZMCTests1/Zmc4SeriesTests.cs
@@ -3,6 +3,7 @@ using APAS.MotionLib.ZMC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,9 +23,13 @@ namespace APAS.MotionLib.ZMC.Tests
 		[TestMethod()]
 		public void ACSMotionTest()
 		{
-			var X = 0;
-			var Y = 1;
-			var acs = new ACS.ACS("10.0.0.100", 701);
+			
+			var portNameSimulator = "SIMULATOR"; // 连接到模拟器
+			var portNameIP = new IPAddress([10, 0, 0, 100]).ToString(); // 连接到真实IP
+			var X = 0; // X轴编号
+			var Y = 1; // Y轴编号
+			// var acs = new ACS.ACS("10.0.0.100", 701);
+			var acs = new ACS.ACS(portNameSimulator, 701);
 			acs.UnitTestMotion(X);
 		}
 	}
