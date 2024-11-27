9fc98766ff815cfc57712894d3aa87ce815b72d9
SL <123@123.com>
2024-11-27 18:09:43 +0800

    fix(ZMC): 修正Home时，如果报警，PollHomeDone无法结束的问题。

--- APAS.MotionLibZMC/ZMC/Zmc4Series.cs
+++ APAS.MotionLibZMC/ZMC/Zmc4Series.cs
@@ -206,39 +206,40 @@ namespace APAS.MotionLib.ZMC
             var rtn = zmcaux.ZAux_Direct_GetIfIdle(_hMc, axis, ref isIdle);
             CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_GetIfIdle));
 
-            var isHomeDone = isIdle == IDLE_FLAG;
-            if (isHomeDone)
-            {
-                var isAlarm = CheckAlarm(axis, out var alarm);
-                if (!isAlarm)
-                {
-                    var movParam = FindAxisConfig(axis, _mcConfig)?.Motion;
-                    if (movParam == null)
-                        throw new NullReferenceException($"unable to find the config of the axis ({axis}).");
+            if (isIdle != IDLE_FLAG)
+                return false;
 
-                    // 清空位置
-                    rtn = zmcaux.ZAux_Direct_SetMpos(_hMc, axis, 0);
-                    CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetMpos));
+            // 如果Idle，检查是否存在错误。
+            var isAlarm = CheckAlarm(axis, out var alarm);
+            if (isAlarm)
+                throw new Exception(alarm.Message); // 报错
 
-                    rtn = zmcaux.ZAux_Direct_SetDpos(_hMc, axis, 0);
-                    CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetDpos));
+            var movParam = FindAxisConfig(axis, _mcConfig)?.Motion;
+            if (movParam == null)
+                throw new NullReferenceException($"unable to find the config of the axis ({axis}).");
 
-                    // 将该轴标记为已Home
-                    rtn = zmcaux.ZAux_Modbus_Set0x(_hMc, (ushort)axis, 1, new byte[] { 1 });
-                    CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Modbus_Set0x));
+            
+            
+            // 清空位置
+            rtn = zmcaux.ZAux_Direct_SetMpos(_hMc, axis, 0);
+            CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetMpos));
 
-                    // 将加速度设置为Move使用的加速度
-                    rtn = zmcaux.ZAux_Direct_SetAccel(_hMc, axis, (float)movParam.Acc);
-                    CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetAccel));
+            rtn = zmcaux.ZAux_Direct_SetDpos(_hMc, axis, 0);
+            CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetDpos));
 
-                    rtn = zmcaux.ZAux_Direct_SetDecel(_hMc, axis, (float)movParam.Dec);
-                    CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetDecel));
+            // 将该轴标记为已Home
+            rtn = zmcaux.ZAux_Modbus_Set0x(_hMc, (ushort)axis, 1, new byte[] { 1 });
+            CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Modbus_Set0x));
 
-                    return true;
-                }
-            }
+            // 将加速度设置为Move使用的加速度
+            rtn = zmcaux.ZAux_Direct_SetAccel(_hMc, axis, (float)movParam.Acc);
+            CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetAccel));
 
-            return false;
+            rtn = zmcaux.ZAux_Direct_SetDecel(_hMc, axis, (float)movParam.Dec);
+            CommandRtnCheck(rtn, nameof(zmcaux.ZAux_Direct_SetDecel));
+
+            return true;
+
         }
 
         /// <summary>
